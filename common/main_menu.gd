891ec5c935a15f15fb092b027ea599e8fd0fb0b9
Ryan Arana <ryan.arana@hey.com>
2023-11-27 23:59:16 -0800

    Add a main menu that iterates over dirs in "res://entries"
    It is expected that the directories in "entries" will be laid out in the same way the AoC website is: YYYY/day/D/input (to get to the input for Day D in Year YYYY). Godot doesn't seem to like files without extensions, so I made the "input" actually be "input.txt".
    
    Currently this menu checks for the existence of input.txt in the Year>Day that is selected and downloads it if it doesn't exist. This requires common/session.txt to have a valid session in it from the AoC cookie.
    
    Next step will be to make it load up the scene for that day as well.

---
+++ common/main_menu.gd
@@ -0,0 +1,55 @@
+extends Control
+
+@onready var button_container: VBoxContainer = %ButtonContainer
+@onready var back_button: Button = %BackButton
+
+const root_path: String = "res://entries"
+
+func _ready() -> void:
+	load_menu(root_path)
+
+func load_menu(path: String) -> void:
+	for child in button_container.get_children():
+		child.queue_free()
+	
+	back_button.visible = path != root_path
+	var dir_iterator := DirAccess.open(path)
+	if dir_iterator:
+		dir_iterator.list_dir_begin()
+		var dir := dir_iterator.get_next()
+		while dir != "":
+			if dir_iterator.current_is_dir():
+				var button := Button.new()
+				button.text = dir
+				if path == root_path:
+					button.button_up.connect(func(): load_menu("%s/" % root_path + "%s/day" % dir))
+				else:
+					button.button_up.connect(func():
+						load_day(path + "/" + dir)
+					)
+				button_container.add_child(button)
+			dir = dir_iterator.get_next()
+	else:
+		print("An error occurred when trying to load menu for %s" % path)
+
+func load_day(path: String) -> void:
+	var file_path := path + "/input.txt"
+	if not FileAccess.file_exists(file_path) or FileAccess.get_file_as_string(file_path).length() == 0:
+		var req := $HTTPRequest as HTTPRequest
+		req.download_file = file_path
+		
+		var url = path.replace(root_path, "https://adventofcode.com") + "/input"
+		var session_path := "res://common/session.txt"
+		if not FileAccess.file_exists(session_path):
+			print("Session not found in %s" % session_path)
+			print("You need to go to adventofcode.com in a browser, log in, and go to an input file (https://adventofcode.com/2022/day/1/input for example) and pull the session from your browser's inspection tab / request headers. Then paste that without the 'session=' into res://common/session.txt. This file is ignored in the .gitignore.")
+			return
+		
+		var cookie = FileAccess.get_file_as_string(session_path).strip_edges()
+		req.request(url, ["Cookie: session=%s" % cookie])
+
+func _on_exit_button_button_up() -> void:
+	get_tree().quit()
+
+func _on_back_button_button_up() -> void:
+	load_menu(root_path)
