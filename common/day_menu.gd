14301556189164cc0dbdc8a4b1243491644fbe6e
Ryan Arana <ryan.arana@hey.com>
2023-11-28 21:10:22 -0800

    Use better DirAccess APIs
    I missed these functions in the last commit and they make it so that the repeated code in the Main Menu and Day Menu aren't worth refactoring to save, it's easy enough to just loop over an array.

--- common/day_menu.gd
+++ common/day_menu.gd
@@ -8,17 +8,10 @@ func _ready() -> void:
 	input_choices.clear()
 	
 	var path := get_tree().current_scene.scene_file_path.get_base_dir()
-	var dir_iterator := DirAccess.open(path)
-	if dir_iterator:
-		dir_iterator.list_dir_begin()
-		var file := dir_iterator.get_next()
-		while file != "":
-			if file.get_extension() == "txt":
-				input_choices.add_item(file.get_file())
-			
-			file = dir_iterator.get_next()
-	else:
-		print("An error occurred when trying to load menu for %s" % path)
+	var files := DirAccess.get_files_at(path)
+	for file in files:
+		if file.get_extension() == "txt":
+			input_choices.add_item(file.get_file())
 
 func _on_back_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://common/main_menu.tscn")
--- common/main_menu.gd
+++ common/main_menu.gd
@@ -13,24 +13,18 @@ func load_menu(path: String) -> void:
 		child.queue_free()
 	
 	back_button.visible = path != root_path
-	var dir_iterator := DirAccess.open(path)
-	if dir_iterator:
-		dir_iterator.list_dir_begin()
-		var dir := dir_iterator.get_next()
-		while dir != "":
-			if dir_iterator.current_is_dir():
-				var button := Button.new()
-				button.text = dir
-				if path == root_path:
-					button.button_up.connect(func(): load_menu("%s/" % root_path + "%s/day" % dir))
-				else:
-					button.button_up.connect(func():
-						load_day(path + "/" + dir)
-					)
-				button_container.add_child(button)
-			dir = dir_iterator.get_next()
-	else:
-		print("An error occurred when trying to load menu for %s" % path)
+	
+	var directories := DirAccess.get_directories_at(path)
+	for dir in directories:
+		var button := Button.new()
+		button.text = dir
+		if path == root_path:
+			button.button_up.connect(func(): load_menu("%s/" % root_path + "%s/day" % dir))
+		else:
+			button.button_up.connect(func():
+				load_day(path + "/" + dir)
+			)
+		button_container.add_child(button)
 
 func load_day(path: String) -> void:
 	var file_path := path + "/input.txt"
