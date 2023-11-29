8309b048bf892880f480823f092ec54344490ba3
Ryan Arana <ryan.arana@hey.com>
2023-11-28 21:21:22 -0800

    Move Input fetch to the Day Menu and make it manually triggered

--- common/day_menu.gd
+++ common/day_menu.gd
@@ -3,8 +3,13 @@ class_name DayMenu
 
 @onready var run_button: Button = %RunButton
 @onready var input_choices: OptionButton = %InputChoices
+@onready var get_input_button: Button = %GetInputButton
+@onready var http_request: HTTPRequest = $HTTPRequest
 
 func _ready() -> void:
+	load_menu()
+
+func load_menu():
 	input_choices.clear()
 	
 	var path := get_tree().current_scene.scene_file_path.get_base_dir()
@@ -15,3 +20,30 @@ func _ready() -> void:
 
 func _on_back_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://common/main_menu.tscn")
+
+func _on_get_input_button_button_up() -> void:
+	var path := get_tree().current_scene.scene_file_path.get_base_dir()
+	var file_path := path + "/input.txt"
+	
+	http_request.download_file = file_path
+	
+	var url = path.replace(MainMenu.root_path, "https://adventofcode.com") + "/input"
+	var session_path := "res://common/session.txt"
+	if not FileAccess.file_exists(session_path):
+		print("Session not found in %s" % session_path)
+		print("You need to go to adventofcode.com in a browser, log in, and go to an input file (https://adventofcode.com/2022/day/1/input for example) and pull the session from your browser's inspection tab / request headers. Then paste that without the 'session=' into res://common/session.txt. This file is ignored in the .gitignore.")
+		return
+	
+	var cookie := "Cookie: session=%s" % FileAccess.get_file_as_string(session_path).strip_edges()
+	print("Downloading input.txt from " + url + " using cookie '%s'" % cookie)
+	http_request.request(url, [cookie])
+
+
+func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	# TODO: Make a Toast system of some kind and show these messages (or something similar) in the UI
+	print("Request completed")
+	if result == HTTPRequest.RESULT_SUCCESS:
+		print("Successfully downloaded %d bytes" % http_request.get_downloaded_bytes())
+		load_menu()
+	else:
+		print("Download failed: %d" % result)
--- common/main_menu.gd
+++ common/main_menu.gd
@@ -1,4 +1,5 @@
 extends Control
+class_name MainMenu
 
 @onready var button_container: VBoxContainer = %ButtonContainer
 @onready var back_button: Button = %BackButton
@@ -27,21 +28,6 @@ func load_menu(path: String) -> void:
 		button_container.add_child(button)
 
 func load_day(path: String) -> void:
-	var file_path := path + "/input.txt"
-	if not FileAccess.file_exists(file_path) or FileAccess.get_file_as_string(file_path).length() == 0:
-		var req := $HTTPRequest as HTTPRequest
-		req.download_file = file_path
-		
-		var url = path.replace(root_path, "https://adventofcode.com") + "/input"
-		var session_path := "res://common/session.txt"
-		if not FileAccess.file_exists(session_path):
-			print("Session not found in %s" % session_path)
-			print("You need to go to adventofcode.com in a browser, log in, and go to an input file (https://adventofcode.com/2022/day/1/input for example) and pull the session from your browser's inspection tab / request headers. Then paste that without the 'session=' into res://common/session.txt. This file is ignored in the .gitignore.")
-			return
-		
-		var cookie = FileAccess.get_file_as_string(session_path).strip_edges()
-		req.request(url, ["Cookie: session=%s" % cookie])
-
 	get_tree().change_scene_to_file(path + "/day.tscn")
 
 func _on_back_button_button_up() -> void:
