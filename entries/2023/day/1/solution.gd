50df4382a3ddd0e1a573a825ef6c8e5bbe29bda1
Ryan Arana <ryan.arana@hey.com>
2023-12-01 09:02:06 -0800

    Solve 2023 Day 1 Part 2

--- entries/2023/day/1/solution.gd
+++ entries/2023/day/1/solution.gd
@@ -4,16 +4,20 @@ var input_dir: String:
 	get:
 		return get_script().resource_path.get_base_dir()
 
+var word_digit_map := {
+	"one": "1",
+	"two": "2",
+	"three": "3",
+	"four": "4",
+	"five": "5",
+	"six": "6",
+	"seven": "7",
+	"eight": "8",
+	"nine": "9"
+}
+
 func part_1(input_filename: String):
-	var lines := get_input_string(input_filename).split("\n")
-	var number_lines = Array(lines).map(func(line: String):
-		var numbers := []
-		for c in line:
-			if c.is_valid_int():
-				numbers.append(c)
-		return numbers
-	)
-	print(number_lines)
+	var number_lines := get_number_lines(input_filename)
 	
 	var numbers := []
 	for line: Array in number_lines:
@@ -22,6 +26,42 @@ func part_1(input_filename: String):
 	
 	var sum: int = numbers.reduce(convert_to_int_and_sum, 0)
 	print(sum)
+
+func part_2(input_filename: String):
+	var number_lines := get_number_lines(input_filename, true).filter(func(line): return not line.is_empty())
+	print(number_lines)
+	var numbers := []
+	for line: Array in number_lines:
+		numbers.append(line.front() + line.back())
+	print(numbers)
+	
+	var sum: int = numbers.reduce(convert_to_int_and_sum, 0)
+	print(sum)
+
+func get_number_lines(input_filename: String, replace_words: bool = false) -> Array:
+	var lines := get_input_string(input_filename).split("\n")
+	var number_lines = Array(lines).map(func(line: String):
+		var numbers := []
+		if replace_words:
+			var inserts := {}
+			for key in word_digit_map:
+				var i = line.find(key)
+				while i >= 0:
+					inserts[i] = word_digit_map[key]
+					i = line.find(key, i + 1)
+			
+			var sorted_inserts = inserts.keys()
+			sorted_inserts.sort()
+			for i in sorted_inserts.size():
+				var original_location = sorted_inserts[i]
+				line = line.insert(original_location + i, inserts[original_location])
+		
+		for c in line:
+			if c.is_valid_int():
+				numbers.append(c)
+		return numbers
+	)
+	return number_lines
 
 func convert_to_int_and_sum(sum: int, n: String) -> int:
 	return sum + int(n)
