b9aed33b4b8b6ef5833764fe7bcc4f5e46d0779e
Ryan Arana <ryan.arana@hey.com>
2023-12-03 01:25:57 -0800

    2023 Day 3 Part 2

--- entries/2023/day/3/solution.gd
+++ entries/2023/day/3/solution.gd
@@ -32,17 +32,45 @@ func get_position(i: int) -> Vector2i:
 	var pos := Vector2i(i % row_width, floor(i / row_width))
 	return pos
 
-func is_part_number(result: RegExMatch) -> bool:
+func is_part_number(result: RegExMatch):
 	for i in range(result.get_start(), result.get_end()):
 		var loc := get_position(i)
 		for y in range(max(loc.y - 1, 0), loc.y + 2):
 			if symbol_locations.has(y):
 				for symbol_x in symbol_locations[y]:
 					if abs(loc.x - symbol_x) <= 1:
-						return true
-	
-	return false
+						return Vector2i(symbol_x, y)
 
 func part_2(input_filename: String) -> String:
-	return "TODO: Solve today's Part 2 using " + input_filename + ". The contents of that file are: '" + get_input_string(input_filename) + "'"
+	var string := get_input_string(input_filename)\
+		.replace("\n", "") # strip newlines, they screw with the coordinate conversion
+	row_width = get_input_lines(input_filename)[0].length()
+	
+	var digit_regex := RegEx.new()
+	digit_regex.compile(r"\d+")
+	var symbol_regex := RegEx.new()
+	symbol_regex.compile(r"\*")
+
+	symbol_locations.clear()
+	var gear_ratios := {}
+	for result in symbol_regex.search_all(string):
+		var pos := get_position(result.get_start())
+		if not symbol_locations.has(pos.y):
+			symbol_locations[pos.y] = []
+		symbol_locations[pos.y].append(pos.x)
+		gear_ratios[pos] = []
+	
+	var sum := 0
+	for result in digit_regex.search_all(string):
+		var gear = is_part_number(result)
+		if gear:
+			gear_ratios[gear].append(int(result.get_string()))
+	
+	for key in gear_ratios:
+		var gear = gear_ratios[key]
+		if gear.size() == 2:
+			sum += gear[0] * gear[1]
+	
+	log_message("Sample answer should be 467835")
+	return str(sum)
 
