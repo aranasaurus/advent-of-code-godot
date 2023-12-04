c3a40689912b9ebe3db35dc0d839f07146ffd61f
Ryan Arana <ryan.arana@hey.com>
2023-12-03 22:59:47 -0800

    2023 Day 4 Part 2

--- entries/2023/day/4/solution.gd
+++ entries/2023/day/4/solution.gd
@@ -12,11 +12,7 @@ func part_1(input_filename: String) -> String:
 		var card_numbers = card.split(" | ")[1].split(" ")
 		card_numbers = parse_numbers(card_numbers)
 		
-		# TODO: Optimize this? Doesn't seem like GDScript has Sets so it'll only be worth it if the performance of this solution is slow enough to make it necessary
-		var number_of_matches := 0
-		for winning_number in winning_numbers:
-			if card_numbers.has(winning_number):
-				number_of_matches += 1
+		var number_of_matches := count_matches(winning_numbers, card_numbers)
 		if number_of_matches > 0:
 			winning_cards.append(number_of_matches)
 		
@@ -25,6 +21,7 @@ func part_1(input_filename: String) -> String:
 		sum += int(pow(2, number_of_matches - 1))
 	
 	log_message("Expected sample answer: 13")
+	log_message("Expected input answer: 32609")
 	return str(sum)
 
 func parse_numbers(numbers: PackedStringArray) -> Array:
@@ -33,5 +30,43 @@ func parse_numbers(numbers: PackedStringArray) -> Array:
 	return arr.map(func(s: String): return int(s))
 
 func part_2(input_filename: String) -> String:
-	return "TODO: Solve today's Part 2 using " + input_filename + ". The contents of that file are: '" + get_input_string(input_filename) + "'"
+	var lines := get_input_lines(input_filename)
+	
+	var copy_map = {}
+	var card_count := 0
+	for card: String in lines:
+		var card_id := int(card.split(": ")[0].replace("Game ", ""))
+		card = card.split(": ")[1]
+		var winning_numbers = card.split(" | ")[0].split(" ")
+		winning_numbers = parse_numbers(winning_numbers)
+		var card_numbers = card.split(" | ")[1].split(" ")
+		card_numbers = parse_numbers(card_numbers)
+		
+		# Count this card's copies
+		var card_copies := 1
+		if copy_map.has(card_id):
+			card_copies += copy_map[card_id]
+			card_count += card_copies
+		else:
+			card_count += 1
+		
+		# Add 1 copy for each following card based on the number of matching numbers on this card
+		var number_of_matches := count_matches(winning_numbers, card_numbers)
+		if number_of_matches > 0:
+			for i in range(number_of_matches):
+				var copy_id = card_id + i + 1
+				if copy_map.has(copy_id):
+					copy_map[copy_id] += card_copies
+				else:
+					copy_map[copy_id] = card_copies
+	
+	log_message("Expected sample answer: 30")
+	log_message("Expected input answer: 14624680")
+	return str(card_count)
 
+func count_matches(winning_numbers: Array, card_numbers: Array) -> int:
+	var number_of_matches := 0
+	for winning_number in winning_numbers:
+		if card_numbers.has(winning_number):
+			number_of_matches += 1
+	return number_of_matches
